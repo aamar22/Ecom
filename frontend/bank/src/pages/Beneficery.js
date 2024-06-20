49d843bbeaecd1bf2de2576e8e94cf69538bda15
aamar22 <51492489+aamar22@users.noreply.github.com>
2024-06-20 10:06:14 +0000

    ui screen

--- frontend/bank/src/pages/Beneficery.js
+++ frontend/bank/src/pages/Beneficery.js
@@ -2,6 +2,41 @@ import React, { useState } from "react";
 import Sidenav from "../Components/Sidenav";
 import Box from '@mui/material/Box';
 import "../Style/style.css"
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+	{ field: 'id', headerName: 'ID', width: 70 },
+	{ field: 'firstName', headerName: 'First name', width: 130 },
+	{ field: 'lastName', headerName: 'Last name', width: 130 },
+	{
+	  field: 'age',
+	  headerName: 'Age',
+	  type: 'number',
+	  width: 90,
+	},
+	{
+	  field: 'fullName',
+	  headerName: 'Full name',
+	  description: 'This column has a value getter and is not sortable.',
+	  sortable: false,
+	  width: 160,
+	  valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+	},
+  ];
+  
+  const rows = [
+	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+	{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 const Beneficiary = () => {
 
 
@@ -10,9 +45,35 @@ const Beneficiary = () => {
 		<div className="test">
 	
 		  <Sidenav/>
-          <h1>Beneficiary</h1>
+         
+		  <div >
+		  <h1>Beneficiary</h1>
+		  <h3>AccountNumber</h3>
+		  <TextField id="outlined-basic" label="AccountNumber" variant="outlined" />
+		  <h3>IFSC</h3>
+		 <TextField id="outlined-basic" label="IFSC" variant="outlined" />
+		 <h3>Beneficiary Name</h3>
+		 <TextField id="outlined-basic" label="Bene Name" variant="outlined" />
+		 <p>payee will add in 30 mins</p>
+		 <Button variant="contained">ADD</Button>
+		  </div>
+		  <h1>Beneficiary List</h1>
+		  <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
 		
 		</div>
+		
 	);
 };
 
--- frontend/bank/src/pages/Quickpay.js
+++ frontend/bank/src/pages/Quickpay.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import Sidenav from "../Components/Sidenav";
 import "../Style/style.css"
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 const Quickpay = () => {
 	return (
-        <div className="test">
+        <div >
       <Sidenav/>
+   <div className="testpay">   
+          <div className="testone">   
           <h1>Quickpay</h1>
+          <p>AccountNumber</p>
+		  <TextField id="outlined-basic" label="AccountNumber" variant="outlined" />
+		  <p>IFSC</p>
+		 <TextField id="outlined-basic" label="IFSC" variant="outlined" />
+
+     </div>
+     <div className="testtwo">
+     <p>Beneficiary Name</p>
+		 <TextField id="outlined-basic" label="Bene Name" variant="outlined" />
+     <p>Amount</p>
+		 <TextField id="outlined-basic" label="Amount" variant="outlined" />
+		 <p>payee will add in 30 mins</p>
+		 <Button variant="contained">Pay now</Button>
+		  </div>
+      </div>
 		</div>
 	);
 };
